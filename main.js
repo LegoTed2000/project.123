cd00a45fa5c5cc948aa8b65c113914eb92c86dac
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-10-04 19:32:55 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftwristX = 0;
+rightwrist = 0;
+difference = 0;
+
+
 function setup() {
     canvas = createCanvas(800, 800);
     canvas.position(1500, 250);
@@ -8,12 +13,26 @@ function setup() {
     poseNet.on("pose", gotPosses);
 }
 
+function draw() {
+    document.getElementById("font_size");
+        background("yellow");
+        text("Hello", 200, 400);
+
+    fill(50, 92, 168);
+    stroke(5, 241, 245);
+    textSize(difference);
+}
+
 function modelloaded() {
     console.log("THE MODEL HAS LOADED!!!!!! YAY =0");
 }
 
 function gotPosses(results) {
-    if(results.length > 0) {
+    if (results.length > 0) {
         console.log(results);
+        leftwristX = results[0].pose.leftWrist.x;
+        rightwristX = results[0].pose.rightWrist.x;
+        difference = floor(leftwristX - rightwristX);
+        console.log("leftwrist = " + leftwristX + "rightwrist = " + rightwristX + "difference = " + difference);
     }
 }
